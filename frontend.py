a1990fac7a03d0ac669161c1f2ef599ef653e94f
petergpt <130558610+petergpt@users.noreply.github.com>
2023-04-30 16:28:39 +0000

    App improvements

--- frontend.py
+++ frontend.py
@@ -5,60 +5,62 @@ from openai_api import transcribe_audio, summarize_text
 from audio_recorder_streamlit import audio_recorder
 import uuid
 from datetime import datetime
+import io
+import wave
 
-def apply_custom_css():
-    custom_css = """
-        <style>
-            .recorder-container .ar-button-container {
-                height: 60px !important;
-            }
-        </style>
-    """
-    st.markdown(custom_css, unsafe_allow_html=True)
+def get_audio_duration(audio_bytes):
+    with io.BytesIO(audio_bytes) as audio_file:
+        with wave.open(audio_file, "rb") as wave_file:
+            frame_rate = wave_file.getframerate()
+            n_frames = wave_file.getnframes()
+            duration = n_frames / float(frame_rate)
+    return duration
 
 def render_frontend():
     create_surveys_table()
-    apply_custom_css()
 
-    st.title("Quick Customer Surveys")
+    st.title("Voice Restaurant Reviews")
 
     user_id = str(uuid.uuid4())
 
-    st.write("Record your voice (up to 3 minutes):")
-    with st.container() as recorder_container:
-        st.write('<div class="recorder-container"></div>', unsafe_allow_html=True)
-        audio_bytes = audio_recorder(
-            text="Click to Record",
-            recording_color="#e8b62c",
-            neutral_color="#6aa36f",
-            icon_name="microphone",
-            icon_size="2x",
-            pause_threshold=None,
-            energy_threshold=None,
-        )
+    st.write("Tell us what you thought in your own voice:")
+    audio_bytes = audio_recorder(
+        text="Click to Record",
+        recording_color="#e8b62c",
+        neutral_color="#6aa36f",
+        icon_name="microphone",
+        icon_size="2x",
+        pause_threshold=None,
+        energy_threshold=(3000, 3000),
+    )
 
     if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-        recording_length = 180  # Assume 3 minutes for now
+        duration = get_audio_duration(audio_bytes)
 
-        with st.spinner("Saving voice recording..."):
-            wav_data, duration = save_voice_recording(audio_bytes, recording_length)
+        if duration < 1:
+            st.warning("The recording is too short. Please record again.")
+        else:
+            st.audio(audio_bytes, format="audio/wav")
+            recording_length = 180  # Assume 3 minutes for now
 
-        with st.spinner("Transcribing voice input..."):
-            transcribed_text = transcribe_audio(wav_data)
+            with st.spinner("Saving voice recording..."):
+                wav_data, duration = save_voice_recording(audio_bytes, recording_length)
 
-        with st.spinner("Summarizing transcribed text..."):
-            summarized_output = summarize_text(transcribed_text)
+            with st.spinner("Transcribing voice input..."):
+                transcribed_text = transcribe_audio(wav_data)
 
-        st.write("Original Transcription:")
-        st.text_area("", transcribed_text, height=100)
+            with st.spinner("Summarizing transcribed text..."):
+                summarized_output = summarize_text(transcribed_text)
 
-        st.write("Summarized Output:")
-        edited_output = st.text_area("", summarized_output, height=100)
+            st.write("Original Transcription:")
+            st.text_area("", transcribed_text, height=100)
 
-        if st.button("Submit"):
-            insert_survey_record(user_id, datetime.utcnow(), duration, wav_data, transcribed_text, edited_output)
-            st.success("Survey submitted successfully!")
+            st.write("Summarized Output:")
+            edited_output = st.text_area("", summarized_output, height=100)
+
+            if st.button("Submit"):
+                insert_survey_record(user_id, datetime.utcnow(), duration, wav_data, transcribed_text, edited_output)
+                st.success("Survey submitted successfully!")
 
     st.markdown("---")
     st.subheader("Previous Surveys")
--- openai_api.py
+++ openai_api.py
@@ -2,6 +2,7 @@ import openai
 import os
 import tempfile
 import base64
+import streamlit as st
 
 
 def transcribe_audio(wav_data):
@@ -10,17 +11,32 @@ def transcribe_audio(wav_data):
     audio_file.write(wav_data)
     audio_file.close()
 
-    with open(audio_file.name, "rb") as file:
-        transcript = openai.Audio.transcribe("whisper-1", file)
+    try:
+        with open(audio_file.name, "rb") as file:
+            transcript = openai.Audio.transcribe("whisper-1", file)
+        os.unlink(audio_file.name)
+        return transcript["text"]
+    except openai.error.InvalidRequestError as e:
+        os.unlink(audio_file.name)
+        st.warning("Audio file is too short. Please record again.")
+        return ""  # Return an empty string or an appropriate message
 
-    os.unlink(audio_file.name)
+def summarize_text(transcribed_text, model="gpt-4"):
+    prompt = f"""
+    You have 2 tasks, write the review and the sentiment.
+    1: Please summarised review of the restaurant based on the transcription of the audio submitted to you by the user. The review should sound as if it was said by the user. Do not answer the text yourself, only provide the summary.
 
-    return transcript["text"]
+    2: Provide a nuanced sentiment of the transcription. Be nuanced in the sentiment, convey the feelings that the user felt.
 
-def summarize_text(prompt, model="gpt-4"):  
+    Follow this format:
+    Review: ...
+    Sentiment:...
+
+    Transcription from the user:\n {transcribed_text}
+    """
     messages = [
-      {"role": "system", "content": "You are restaurant reviewer GPT. You write short succinct reviews based on the transcripts of audio submitted by the user. You wrtie reviews from the first person. You do not engage with the content of the message"},
-      {"role": "user", "content": prompt}
+        {"role": "system", "content": "You are restaurant reviewer GPT. You write short succinct reviews based on the transcripts of audio submitted by the user. You write reviews from the first person. You do not engage with the content of the message"},
+        {"role": "user", "content": prompt}
     ]
     response = openai.ChatCompletion.create(
         model=model,
@@ -29,18 +45,3 @@ def summarize_text(prompt, model="gpt-4"):
         temperature=0.7, # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
-
-prompt = f"""
-You have 2 tasks, write the review and the sentiment.
-1: Please summarised review of the restaurant based on the transcription of the audio submitted to you by the user. The review should sound as if it was said by the user. Do not answer the text yourself, only provide the summary.
-
-2: Provide a neuanced sentiment of the transcription. Be neuanced in the sentiment, convey the feelings that the user felt.
-
-Follow this format:
-Review: ...
-Sentiment:...
-
-Transcription from the user:\n {"text"}
-"""
-
-response = summarize_text(prompt)
